d7952877a2b3eedf6d7870a43e1658d26a891d9e
Danis <ndexx217@gmail.com>
2025-08-03 13:03:25 +0300

    Initial commit

---
+++ src/main/java/ru/netology/services/VacationService.java
@@ -0,0 +1,22 @@
+package ru.netology.services;
+
+public class VacationService {
+
+    public int calculate(int income, int expenses, int threshold) {
+        int count = 0; // счётчик месяцев отдыха
+        int money = 0; // количество денег на счету
+        for (int month = 0; month < 12; month++) {
+            if(money<threshold){
+                money+=income;
+                money-=expenses;
+            }
+            else{
+                count++;
+                money-=expenses;
+                money/=3;
+            }
+
+        }
+        return count;
+    }
+}
---
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -0,0 +1,19 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class VacationServiceTest {
+
+    @ParameterizedTest
+    @CsvFileSource(files ="src/test/resources/VacationTest.csv")
+    public void testVecationService(int expected, int income, int expenses, int threshold){
+        VacationService service = new VacationService();
+
+        int actual = service.calculate(income,expenses,threshold);
+
+        Assertions.assertEquals(expected,actual);
+    }
+
+}
